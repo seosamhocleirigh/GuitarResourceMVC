6b09fa45ead68cd09706d5548ce0066465596684
Wolfieselket <patrick7996@gmail.com>
2017-03-31 14:20:18 +0100

    Added inputboxes and button for sheet music tool

--- GuitarResourceWebApplication/GuitarResourceWebApplication/Controllers/ISheetMusicLearningTool.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.Mvc;
-
-namespace GuitarResourceWebApplication.Controllers
-{
-    public interface ISheetMusicLearningTool
-    {
-        ActionResult Index();
-    }
-}
---
+++ GuitarResourceWebApplication/GuitarResourceWebApplication/Controllers/SheetMusicLearningToolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuitarResourceWebApplication.Controllers
+{
+    public class SheetMusicLearningToolController : Controller
+    {
+        // GET: SheetMusicLearningTool
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
