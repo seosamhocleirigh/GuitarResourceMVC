64f1050bc6914b4f5ee5f1c38e79e9af5f0d69a4
Wolfieselket <patrick7996@gmail.com>
2017-04-08 13:02:02 +0100

    Work on generator checkboxes

--- GuitarResourceWebApplication/GuitarResourceWebApplication/Scripts/GuitarResourceLibrary/RandomNoteGenerator.js
+++ GuitarResourceWebApplication/GuitarResourceWebApplication/Scripts/GuitarResourceLibrary/RandomNoteGenerator.js
@@ -1,37 +1,50 @@
 ﻿
-    var RandomNoteGenerator = {
+var RandomNoteGenerator = {
 
 
-        noteArrayCopy:[],
+    noteArrayCopy: [],
+    naturalArrayCopy: [],
+    stringsArrayCopy: [],
 
-        defaults: {
-            buttonNextNote: $("#buttonrandomnote"),
-            randomNoteOutputElement: $("#outputboxnotes"),
-            randomStringOutputElement: $("#outputboxstring")
+    defaults: {
+        buttonNextNote: $("#buttonrandomnote"),
+        randomNoteOutputElement: $("#outputboxnotes"),
+        randomStringOutputElement: $("#outputboxstring")
 
-        },
-        
-        init: function (settings) {
-            console.log("Random note generator initialising");
-            $.extend(RandomNoteGenerator.defaults, settings);
-            RandomNoteGenerator.noteArrayCopy = RandomNoteLibrary.copyNoteArray();
-            RandomNoteGenerator.naturalArrayCopy = RandomNoteLibrary.copyNaturalArray();
-            RandomNoteGenerator.stringsArrayCopy = RandomNoteLibrary.copyStringsArray();
-            RandomNoteGenerator.bindUIActions();
-        },
+    },
 
-        bindUIActions: function () {
-            console.log("binding UI");
-            $(RandomNoteGenerator.defaults.buttonNextNote.selector).click(function () {
-                RandomNoteGenerator.outputRandomNoteToUI();
-            })
-        },
+    init: function (settings) {
+        console.log("Random note generator initialising");
+        $.extend(RandomNoteGenerator.defaults, settings);
+        RandomNoteGenerator.noteArrayCopy = RandomNoteLibrary.copyNoteArray();
+        RandomNoteGenerator.naturalArrayCopy = RandomNoteLibrary.copyNaturalArray();
+        RandomNoteGenerator.stringsArrayCopy = RandomNoteLibrary.copyStringsArray();
+        RandomNoteGenerator.bindUIActions();
+    },
 
-        outputRandomNoteToUI: function () {
-            console.log("output to UI fired");
+    bindUIActions: function () {
+        console.log("binding UI");
+        $(RandomNoteGenerator.defaults.buttonNextNote.selector).click(function () {
+            RandomNoteGenerator.outputRandomNoteToUI();
+        })
+    },
+
+    outputRandomNoteToUI: function () {
+        console.log("output to UI fired");
+        if ($("[name='natural notes']").prop("checked")) {
+            var randomNote = RandomNoteLibrary.generateRandomNote(RandomNoteGenerator.naturalArrayCopy);
+            $(RandomNoteGenerator.defaults.randomNoteOutputElement.selector).text(randomNote);
+        } else {
             var randomNote = RandomNoteLibrary.generateRandomNote(RandomNoteGenerator.noteArrayCopy);
             $(RandomNoteGenerator.defaults.randomNoteOutputElement.selector).text(randomNote);
-            if (RandomNoteGenerator.noteArrayCopy.length === 0) 
-                RandomNoteGenerator.noteArrayCopy = RandomNoteLibrary.copyNoteArray();
         }
-    };
+        if (RandomNoteGenerator.noteArrayCopy.length === 0)
+            RandomNoteGenerator.noteArrayCopy = RandomNoteLibrary.copyNoteArray();
+    }
+};
+  
+
+
+
+        
+ 
