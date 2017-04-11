213f2d0d496477b277cfc9520311b78db0183bc3
Wolfieselket <patrick7996@gmail.com>
2017-04-11 12:02:18 +0100

    sheet music js work in progress

---
+++ GuitarResourceWebApplication/GuitarResourceWebApplication/Scripts/GuitarResourceLibrary/Common/SheetMusicFunctions.js
@@ -0,0 +1,40 @@
+﻿
+var SheetMusicLibrary = {
+
+    noteArray = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"],
+    stringsArray = ["1", "2", "3", "4", "5", "6"]
+
+
+    copyNoteArray: function () {
+        return RandomNoteLibrary.noteArray.slice();
+    },
+    copyStringsArray: function () {
+        return RandomNoteLibrary.stringsArray.slice();
+    },
+
+    pickRandomItemFromArray: function (array) {
+        var itemIndex = Math.floor(Math.random() * array.length);
+        return array[itemIndex];
+    },
+
+    spliceRandomItemFromArray: function (array) {
+        var randomItem = SheetMusicLibrary.pickRandomItemFromArray(array);
+        var index = array.indexOf(randomItem);
+        array.splice(index, 1);
+        return randomItem;
+    }
+
+    generateRandomstring: function (array) {
+        var string = SheetMusicLibrary.spliceRandomItemFromArray(stringsArrayCopy);
+        resetstringsArray();
+        return string;
+    }
+
+    generateRandomNoteAndDrawOnStave: function (array) {
+        var string = generateRandomstring();
+        var note = generateRandomNote();
+        clearCanvas();
+        drawNoteOnStave(note, string);
+    }
+
+};
